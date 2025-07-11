2161ba084e893b7f3790ef4b8526d808c55970d6
laxmanp23 <laxmanpawar4675@gmail.com>
2025-07-11 10:08:27 +0530

    Fix: clean install for Vercel build

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
                 <div className="absolute inset-0 bg-black/60 dark:bg-white/5 backdrop-blur-sm"></div>
                 <div className="relative z-10 flex items-center justify-center h-full px-4 py-20 text-center">
                     <div className="max-w-4xl mx-auto text-white">
-                        <div className="absolute top-20 right-7 w-8 h-8 rounded-full border-blue-600 animate-blink"></div>
+                        <div className="absolute top-20 right-7  w-8 h-8 rounded-full border-blue-600 animate-blink"></div>
 
                         <h1 className="font-bold text-4xl md:text-6xl dark:text-black "> Welcome to <span className="text-blue-500">Magic Face </span></h1>
                         <p className="text-lg py-2 mb-4 dark:text-black">upload two pictures and watch and wait Magic AI</p>
